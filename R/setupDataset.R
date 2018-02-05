d02bfc5495aa891bb21f85efe14a08cad728b030
AngelBerihuete <angel.berihuete@uca.es>
2018-02-05 09:50:08 +0100

    Changes in SetupDataset redefining default values of some arguments

--- R/setupDataset.R
+++ R/setupDataset.R
@@ -6,9 +6,9 @@
 #'
 #' @param dataset a data.frame containing variables in the EU-SILC microdata format.
 #' @param country a character string specifying the country whose data will be considered.
-#' @param region a character string specifying the region of the country whose data will be considered.
-#' @param s either a character string or a numeric value between 0 and 1 specifying the equivalence scale to be used to obtain the equivalized disposable income. The default ("OECD") considers the standar modified OECD scale.
-#' @param deflac numeric; a number to be used as a deflator. The default (NULL) will not apply any deflation.
+#' @param region a character/vector string specifying the region of the country whose data will be considered. The default (NULL) considers all regions in the country.
+#' @param s a numeric value between 0 and 1 specifying the equivalence scale to be used to obtain the equivalized disposable income. The default (NULL) considers the standar modified OECD scale.
+#' @param deflator numeric; a number to be used as a deflator. The default (NULL) will not apply any deflation.
 #' @param pppr the purchasing power parity rate (PPPR) will be used. Default is NULL.
 #'
 #' @details We obtain the equivalized disposable income with the equivalence
@@ -26,7 +26,7 @@
 #'  \item HX040 an integer vector containing information about households size.
 #'  \item HX050 a numeric vector containing information about the equivalised household size. The scale employed is the modified OECD scale.
 #'  \item HX090 a numeric vector containing information about equivalised disposable income (with the modified OECD scale).
-#'  \item ipuc a numeric vector containing the income per unit of consumption. This variable takes into account if deflac is not NULL, ppp is TRUE or/and the value assigned to \emph{s}.
+#'  \item ipuc a numeric vector containing the income per unit of consumption. This variable takes into account if deflator is not NULL, ppp is TRUE or/and the value assigned to \emph{s}.
 #'  \item wHX040 a numeric vector which is obtained by multiplying DB090 by HX040. It represents household weights taking into account the size of the household.
 #' }
 #'
@@ -43,9 +43,9 @@
 
 setupDataset <- function(dataset,
                          country = 'ES' ,
-                         region = 'all',
-                         s = 'OECD',
-                         deflac = NULL,
+                         region = NULL,
+                         s = NULL,
+                         deflator = NULL,
                          pppr = NULL) {
 
   # The following line is only to overcome the note obtained by
@@ -58,12 +58,8 @@ setupDataset <- function(dataset,
     stop("The variable country is mandatory")
   }
 
-  if(!is.null(region)){ # only for one region
-    if(all(region != 'all')){ # only for one region
+  if(!is.null(region)){ # filter the region(s)
       dataset <- subset(dataset, DB040 %in% region)
-    }
-  }else{
-    stop("The variable region is mandatory")
   }
 
   dataset <- dataset[complete.cases(dataset),]
@@ -72,12 +68,12 @@ setupDataset <- function(dataset,
     dataset$HX090 <- dataset$HX090/pppr
   }
 
-  if(!is.null(deflac)){ # Deflation
-    dataset$HX090 <- dataset$HX090/deflac
+  if(!is.null(deflator)){ # Deflation
+    dataset$HX090 <- dataset$HX090/deflator
   }
 
   # income per unit of consumption
-  if(s == "OECD"){
+  if(is.null(s)){
     dataset$ipuc <- dataset$HX090
   }else{
     dataset$ipuc <- (dataset$HX090*dataset$HX050)/dataset$HX040^s
